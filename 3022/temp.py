dd3c6730267ec25ac081e5640277edfab886911b
Kiryuuin <doraemonstichisnoon@gmail.com>
2026-07-15 14:42:02 +0700

    unfinished temp

---
+++ 3022/temp.py
@@ -0,0 +1,10 @@
+"""temp calc"""
+
+init_temp = float(input())
+temp_type = input()
+final_type = input()
+
+if temp_type == "K":
+    if final_type == "C":
+        result = init_temp + 273.15
+    elif final_type == ""
