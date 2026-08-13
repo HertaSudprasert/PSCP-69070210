95b288d96d26ad3b054f4b20b361eab1f36673fa
Kiryuuin <doraemonstichisnoon@gmail.com>
2026-08-13 22:41:44 +0700

    nigga

--- 3034/pod.py
+++ 3034/pod.py
@@ -7,6 +7,14 @@ N = int(set1.split()[0])
 #K lines, pod capacity and the pod will มารับ when people of K แถวs appear
 K = int(set1.split()[1])
 
-for _ in range (0, N):
-    thingamabob = int(input())
+line = []
 
+for _ in range(0, N):
+    thingamabob = int(input())
+    line.append(thingamabob)
+
+while all(people in line for people in range(1, K + 1)):
+    for people in range(1, K + 1):
+        line.remove(people)
+
+print(len(line))
