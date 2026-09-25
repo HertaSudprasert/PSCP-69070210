56750c4cbf06d87c12b834849bd3e9e2cfd60371
Kiryuuin <doraemonstichisnoon@gmail.com>
2026-09-25 19:49:58 +0700

    logs

--- 3298/buu.py
+++ 3298/buu.py
@@ -2,21 +2,31 @@
 
 thing = input()
 
+MAX_U = 0
+
+def check_thing(substr: str):
+    """ddddd"""
+    global MAX_U
+    ct = 0
+    for char in substr:
+        if char.upper() == "U":
+            ct += 1
+            if ct > MAX_U:
+                MAX_U = ct
+        else:
+            ct = 0
+            return
+
 if "BUU" in thing.upper():
-    counter = 0
-    max_u = 0
     for i, j in enumerate(thing):
-        print(i, j)
         if j.upper() == "B":
-            for char in thing[i:]:
-                
-                if char.upper() == "U":
-                    counter += 1
-                    if counter > max_u:
-                        max_u = counter
-                else:
-                    counter = 0
-
-            
-
-print(max_u)
+            check_thing(thing[i + 1:])
+    print("Yes", MAX_U)
+elif "B" in thing.upper():
+    index = thing.upper().find("B")
+    print(thing[:index] + ("U" * (len(thing) - (index + 1))))
+else:
+    buu = ["B", "U", "U"]
+    for i in range(0, len(thing)):
+        print(buu[i % 3], end="")
+    print()
